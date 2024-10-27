6a7d78386a5221ca7d391df15f87d5ad90da6ba0
MaleeqXT <maleeqxjuttyrt@gmail.com>
2024-10-27 19:18:44 +0500

    add code in login and register

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,73 +1,308 @@
-<!DOCTYPE html>
+<!doctype html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Login</title>
-    <!-- Bootstrap CSS (optional, for enhanced styling) -->
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet">
-    <!-- Add custom styles here -->
+    <meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Login Now</title>
+    <script src="https://cdn.jsdelivr.net/npm/particles.js@2.0.0/particles.min.js"></script>
+
     <style>
-        body { background-color: #f8f9fa; }
-        .card { box-shadow: 0 4px 8px rgba(0, 0, 0, 0.1); }
-        .container { margin-top: 50px; }
+
+@import url("{{asset('assets/img/bg/login-bg.jpg')}}");
+
+        *, *::before, *::after {
+            margin: 0;
+            padding: 0;
+            box-sizing: border-box;
+            font-family: "Quicksand", sans-serif;
+        }
+
+        body {
+            display: flex;
+            justify-content: center;
+            align-items: center;
+            min-height: 100vh;
+            width: 100%;
+            background: black;
+            padding: 40px 20px;
+        }
+
+        /* Snow */
+        #particles-js {
+            position: absolute;
+            width: 100%;
+            height: 100%;
+            z-index: -1;
+        }
+
+        main {
+            display: grid;
+            grid-template-columns: 45% 55%;
+            place-items: center;
+            background: #f6f6f6;
+            width: min(700px, 95%);
+            border-radius: 20px;
+        }
+
+        /* Left Side */
+        .left-side {
+            height: 100%;
+            width: 100%;
+            background-image: url({{ asset('assets/img/bg/login-bg.jpg') }});
+            background-repeat: no-repeat;
+            background-size: cover;
+            background-position: center;
+            pointer-events: none;
+            border-radius: 20px 0 0 20px;
+        }
+
+        /* Right Side - Centering Content */
+        .right-side {
+            padding: 60px;
+            display: flex;
+            flex-direction: column;
+            justify-content: center;
+            align-items: center;
+            height: 100%;
+            text-align: center;
+        }
+
+        /* Form Container Centering */
+        form {
+            width: 100%;
+            max-width: 300px;
+            display: flex;
+            flex-direction: column;
+            align-items: center;
+        }
+
+        /* Logo Container */
+        .logo-container {
+            display: flex;
+            justify-content: center;
+            padding-top: 3rem;
+            padding-bottom: 2rem;
+        }
+
+        /* Logo Image */
+        .logo {
+            color: #ffffff;
+            font-weight: bold;
+            font-size: 1.25rem;
+            padding: 1rem;
+            text-decoration: none;
+        }
+        
+        .logo img {
+            width: 8rem; /* Adjust as needed for responsiveness */
+            max-width: 100%;
+            height: auto;
+        }
+
+        /* Button Group */
+        .btn-group {
+            display: flex;
+            flex-direction: row;
+            justify-content: space-between;
+            align-items: center;
+            gap: 5px;
+            margin-bottom: 32px;
+        }
+
+        .btn-group .btn {
+            display: flex;
+            align-items: center;
+            column-gap: 4px;
+            width: max-content;
+            font-size: 0.8rem;
+            font-weight: 500;
+            padding: 8px 6px;
+            border: 2px solid #6b7280;
+            border-radius: 5px;
+            background-color: #f6f6f6;
+            transform: scale(1);
+            cursor: pointer;
+            transition: transform 0.1s ease, background-color 0.5s, color 0.5s;
+        }
+
+        .btn-group .btn:focus {
+            transform: scale(0.97);
+        }
+
+        .btn-group .btn:hover {
+            background-color: #000;
+            color: #eee;
+        }
+
+        .btn img {
+            width: 16px;
+            height: 16px;
+        }
+
+        /* OR */
+        .or {
+            position: relative;
+            text-align: center;
+            margin-bottom: 24px;
+            font-size: 1rem;
+            font-weight: 600;
+        }
+
+        .or::before, .or::after {
+            content: "";
+            position: absolute;
+            top: 50%;
+            width: 40%;
+            height: 1px;
+            background: #000;
+        }
+
+        .or::before {
+            left: 0;
+        }
+
+        .or::after {
+            right: 0;
+        }
+
+        /* Inputs and Labels */
+        input {
+            width: 100%;
+            padding: 12px 20px;
+            border: 2px solid #ccc;
+            outline: 0;
+            border-radius: 5px;
+            background-color: transparent;
+            margin: 4px 0 18px;
+            font-size: 0.85rem;
+            font-weight: 600;
+            transition: all 0.5s;
+        }
+
+        input:focus {
+            border: 2px solid #000;
+        }
+
+        input:-webkit-autofill, input:-webkit-autofill:hover, input:-webkit-autofill:focus, input:-webkit-autofill:active {
+            -webkit-background-clip: text;
+            -webkit-text-fill-color: #000;
+            transition: background-color 5000s ease-in-out 0s;
+        }
+
+        label {
+            font-weight: 600;
+            font-size: 0.9rem;
+        }
+
+        /* Login Button */
+        .login-btn {
+            width: 100%;
+            font-size: 0.9rem;
+            font-weight: 600;
+            padding: 8px 24px;
+            margin: 12px 0 24px;
+            border: 2px solid #6b7280;
+            border-radius: 5px;
+            background-color: #f6f6f6;
+            cursor: pointer;
+            transition: all 0.5s;
+        }
+
+        .login-btn:hover {
+            background-color: #000;
+            color: #eee;
+        }
+
+        /* Links */
+        .links {
+            display: flex;
+            flex-direction: row;
+            justify-content: space-between;
+        }
+
+        a {
+            color: #000;
+            font-size: 0.88rem;
+            font-weight: 600;
+            letter-spacing: -1px;
+            transition: all 0.4s ease;
+            text-decoration: none;
+        }
+
+        a:hover {
+            color: #B48E40;
+        }
+
+        @media (max-width: 768px) {
+            main {
+                grid-template-columns: 100%;
+            }
+
+            .left-side {
+                display: none;
+            }
+
+            .right-side {
+                padding: 40px 20px;
+            }
+        }
     </style>
 </head>
 <body>
-    <div class="container">
-        <div class="row justify-content-center">
-            <div class="col-md-8">
-                <div class="card">
-                    <div class="card-header">{{ __('Login') }}</div>
-                    <div class="card-body">
-                        <form method="POST" action="{{ route('login') }}">
-                            @csrf
-                            <div class="row mb-3">
-                                <label for="email" class="col-md-4 col-form-label text-md-end">{{ __('Email Address') }}</label>
-                                <div class="col-md-6">
-                                    <input id="email" type="email" class="form-control @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" required autocomplete="email" autofocus>
-                                    @error('email')
-                                        <span class="invalid-feedback" role="alert">
-                                            <strong>{{ $message }}</strong>
-                                        </span>
-                                    @enderror
-                                </div>
-                            </div>
-                            <div class="row mb-3">
-                                <label for="password" class="col-md-4 col-form-label text-md-end">{{ __('Password') }}</label>
-                                <div class="col-md-6">
-                                    <input id="password" type="password" class="form-control @error('password') is-invalid @enderror" name="password" required autocomplete="current-password">
-                                    @error('password')
-                                        <span class="invalid-feedback" role="alert">
-                                            <strong>{{ $message }}</strong>
-                                        </span>
-                                    @enderror
-                                </div>
-                            </div>
-                            <div class="row mb-3">
-                                <div class="col-md-6 offset-md-4">
-                                    <div class="form-check">
-                                        <input class="form-check-input" type="checkbox" name="remember" id="remember" {{ old('remember') ? 'checked' : '' }}>
-                                        <label class="form-check-label" for="remember">{{ __('Remember Me') }}</label>
-                                    </div>
-                                </div>
-                            </div>
-                            <div class="row mb-0">
-                                <div class="col-md-8 offset-md-4">
-                                    <button type="submit" class="btn btn-primary">{{ __('Login') }}</button>
-                                    @if (Route::has('password.request'))
-                                        <a class="btn btn-link" href="{{ route('password.request') }}">{{ __('Forgot Your Password?') }}</a>
-                                    @endif
-                                </div>
-                            </div>
-                        </form>
-                    </div>
-                </div>
-            </div>
-        </div>
-    </div>
+<div id="particles-js" class="snow"></div>
 
-    <!-- Bootstrap JS (optional) -->
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.bundle.min.js"></script>
+<main>
+    <div class="left-side"></div>
+    <div class="right-side">
+        <form method="post" action="{{ route('login') }}">
+            @csrf
+            @if ($errors->any())
+                <div class="alert alert-danger">
+                    <ul>
+                        @foreach ($errors->all() as $error)
+                            <li>{{ $error }}</li>
+                        @endforeach
+                    </ul>
+                </div>
+            @endif
+            <div class="logo-container">
+                <a href="/" class="logo">
+                    <img src="{{ asset('assets/images/21477.jpg') }}" alt="Logo">
+                </a>
+            </div>
+
+            <label for="email">Email</label>
+            <input type="text" placeholder="Enter Email" name="email" required />
+
+            <label for="password">Password</label>
+            <input type="password" placeholder="Enter Password" name="password" required />
+
+            <button type="submit" class="login-btn">Sign in</button>
+            <div class="links">
+                <a href="#">Forgot password?</a>
+                <a href="/register">Do not have an account?</a>
+            </div>
+        </form>
+    </div>
+</main>
+
+<script>
+    particlesJS("particles-js", {
+        particles: {
+            number: {
+                value: 310,
+                density: { enable: true, value_area: 800 },
+            },
+            color: { value: "#fff" },
+            shape: { type: "circle", polygon: { nb_sides: 5 } },
+            opacity: { value: 1 },
+            size: { value: 3, random: true },
+            line_linked: { enable: false },
+            move: { enable: true, speed: 2, direction: "bottom", out_mode: "out" },
+        },
+        retina_detect: true,
+    });
+</script>
 </body>
 </html>
--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -1,80 +1,284 @@
-<!DOCTYPE html>
+<!doctype html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Register</title>
-    <!-- Bootstrap CSS (optional for enhanced styling) -->
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet">
-    <!-- Custom styles -->
+    <meta name="viewport"
+        content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link href="{{asset('vendors/bootstrap/dist/css/bootstrap.min.css')}}" rel="stylesheet">
+
+    <title>Register Now</title>
     <style>
-        body { background-color: #f8f9fa; }
-        .card { box-shadow: 0 4px 8px rgba(0, 0, 0, 0.1); }
-        .container { margin-top: 50px; }
+    @import url('https://fonts.googleapis.com/css2?family=Caveat:wght@400..700&display=swap');
+
+    * {
+        margin: 0;
+        padding: 0;
+        box-sizing: border-box;
+    }
+
+    body {
+        background-color: #ffffff;
+        font-family: 'Karla', sans-serif;
+        height: 100vh;
+        margin: 0;
+        display: flex;
+        justify-content: center;
+        align-items: center;
+    }
+
+    .container1 {
+        width: 100%;
+        display: flex;
+        flex-wrap: wrap;
+    }
+
+    .register-section {
+        width: 100%;
+        height: 100vh;
+        overflow: auto;
+        display: flex;
+        flex-direction: column;
+        justify-content: center;
+        /*/ Firefox /*/
+        scrollbar-width: thin;
+        scrollbar-color: black #f0f0f0;
+    }
+
+    /*/ Webkit Browsers /*/
+    .register-section::-webkit-scrollbar {
+        width: 12px;
+        height: 12px;
+    }
+
+    .register-section::-webkit-scrollbar-thumb {
+        background-color: black;
+        border-radius: 10px;
+    }
+
+    .register-section::-webkit-scrollbar-track {
+        background: #f0f0f0;
+        border-radius: 10px;
+    }
+
+
+    .logo-container {
+        display: flex;
+        justify-content: center;
+        padding-top: 3rem;
+        z-index: 9999;
+    }
+
+    .logo {
+        color: #ffffff;
+        font-weight: bold;
+        font-size: 1.25rem;
+        padding: 1rem;
+        text-decoration: none;
+    }
+
+    .logo img {
+        width: 10rem;
+    }
+
+    .form-container {
+        display: flex;
+        flex-direction: column;
+        justify-content: center;
+        padding-top: 2rem;
+        padding-left: 2rem;
+        padding-right: 2rem;
+    }
+
+    .form-title {
+        text-align: center;
+        font-size: 3rem;
+        font-family: "Caveat", cursive;
+        font-optical-sizing: auto;
+        font-weight: 700;
+        font-style: normal;
+    }
+
+    .register-form {
+        display: flex;
+        flex-direction: column;
+        padding-top: 1.5rem;
+    }
+
+    .form-group {
+        display: flex;
+        flex-direction: column;
+        padding-top: 1rem;
+    }
+
+    .form-label {
+        font-size: 1.125rem;
+    }
+
+    .form-input {
+        box-shadow: 0 0 5px rgba(0, 0, 0, 0.1);
+        border: 1px solid #cbd5e0;
+        border-radius: 0.375rem;
+        width: 100%;
+        padding: 0.5rem 0.75rem;
+        color: #4a5568;
+        margin-top: 0.25rem;
+        transition: box-shadow 0.2s ease-in-out;
+    }
+
+    .form-input:focus {
+        outline: none;
+        box-shadow: 0 0 10px rgba(180, 142, 64, 0.5);
+    }
+
+    .submit-button {
+        background-color: #000000;
+        color: #ffffff;
+        font-weight: bold;
+        font-size: 1.125rem;
+        padding: 0.5rem;
+        margin-top: 2rem;
+        transition: background-color 0.2s ease-in-out;
+        cursor: pointer;
+        border: none;
+        border-radius: 5px;
+        /*font-family: "Caveat", cursive;*/
+    }
+
+    .submit-button:hover {
+        background-color: #B48E40;
+
+    }
+
+    .login-link-container {
+        text-align: center;
+        padding-top: 3rem;
+        padding-bottom: 3rem;
+    }
+
+    .login-link {
+        text-decoration: underline;
+        font-weight: 600;
+        color: #B48E40;
+    }
+
+    .image-section {
+        width: 50%;
+        box-shadow: 0 0 10px rgba(0, 0, 0, 0.5);
+        background-color: black;
+    }
+
+    .background-image {
+        object-fit: cover;
+        width: 100%;
+        height: 100vh;
+        margin-left: 2px;
+        display: none;
+        object-position: right;
+    }
+
+    .text-danger{
+        color:red;
+        padding:5px;
+    }
+    @media (min-width: 768px) {
+        .register-section {
+            width: 50%;
+        }
+
+        .logo-container {
+            justify-content: flex-start;
+            /*padding-top: 3rem;*/
+            padding-left: 3rem;
+            margin-bottom: -3rem;
+
+        }
+
+        .form-container {
+            padding-top: 4rem;
+            padding-left: 4rem;
+            padding-right: 4rem;
+        }
+
+        .background-image {
+            display: block;
+        }
+    }
     </style>
 </head>
-<body>
-    <div class="container">
-        <div class="row justify-content-center">
-            <div class="col-md-8">
-                <div class="card">
-                    <div class="card-header">{{ __('Register') }}</div>
-                    <div class="card-body">
-                        <form method="POST" action="{{ route('register') }}">
-                            @csrf
-                            <div class="row mb-3">
-                                <label for="name" class="col-md-4 col-form-label text-md-end">{{ __('Name') }}</label>
-                                <div class="col-md-6">
-                                    <input id="name" type="text" class="form-control @error('name') is-invalid @enderror" name="name" value="{{ old('name') }}" required autocomplete="name" autofocus>
-                                    @error('name')
-                                        <span class="invalid-feedback" role="alert">
-                                            <strong>{{ $message }}</strong>
-                                        </span>
-                                    @enderror
-                                </div>
-                            </div>
-                            <div class="row mb-3">
-                                <label for="email" class="col-md-4 col-form-label text-md-end">{{ __('Email Address') }}</label>
-                                <div class="col-md-6">
-                                    <input id="email" type="email" class="form-control @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" required autocomplete="email">
-                                    @error('email')
-                                        <span class="invalid-feedback" role="alert">
-                                            <strong>{{ $message }}</strong>
-                                        </span>
-                                    @enderror
-                                </div>
-                            </div>
-                            <div class="row mb-3">
-                                <label for="password" class="col-md-4 col-form-label text-md-end">{{ __('Password') }}</label>
-                                <div class="col-md-6">
-                                    <input id="password" type="password" class="form-control @error('password') is-invalid @enderror" name="password" required autocomplete="new-password">
-                                    @error('password')
-                                        <span class="invalid-feedback" role="alert">
-                                            <strong>{{ $message }}</strong>
-                                        </span>
-                                    @enderror
-                                </div>
-                            </div>
-                            <div class="row mb-3">
-                                <label for="password-confirm" class="col-md-4 col-form-label text-md-end">{{ __('Confirm Password') }}</label>
-                                <div class="col-md-6">
-                                    <input id="password-confirm" type="password" class="form-control" name="password_confirmation" required autocomplete="new-password">
-                                </div>
-                            </div>
-                            <div class="row mb-0">
-                                <div class="col-md-6 offset-md-4">
-                                    <button type="submit" class="btn btn-primary">
-                                        {{ __('Register') }}
-                                    </button>
-                                </div>
-                            </div>
-                        </form>
+
+<body class="bg-white font-family-karla h-screen">
+    <div class="container1">
+        <!-- Register Section -->
+        <div class="register-section">
+
+            <div class="logo-container">
+                <a href="/" class="logo"><img src="{{ asset('assets/images/21477.jpg') }}"></a>
+            </div>
+
+            <div class="form-container">
+                <p class="form-title">Register Now</p>
+                <form class="register-form" action="{{route('register')}}" method="post">
+                    @csrf
+                 
+                    <div class="form-group">
+                        <label for="name" class="form-label">Name</label>
+                        <input type="text" id="name" name="name" placeholder="John Smith" class="form-input" value="{{ old('name') }}"/>
+                        @if ($errors->has('name'))
+                        <span class="text-danger">{{ $errors->first('name') }}</span>
+                        @endif
                     </div>
+
+                    <div class="form-group">
+                        <label for="email" class="form-label">Email</label>
+                        <input type="email" id="email" name="email" placeholder="your@email.com" class="form-input" value="{{ old('email') }}"/>
+                        @if ($errors->has('email'))
+                        <span class="text-danger">{{ $errors->first('email') }}</span>
+                        @endif
+                    </div>
+
+                  
+
+                    <div class="form-group">
+                        <label for="password" class="form-label">Password</label>
+                        <input type="password" id="password" name="password" placeholder="Password"
+                            class="form-input" />
+                        @if ($errors->has('password'))
+                        <span class="text-danger">{{ $errors->first('password') }}</span>
+                        @endif
+                    </div>
+
+                    <div class="form-group">
+                        <label for="confirm-password" class="form-label">Confirm Password</label>
+                        <input type="password" id="confirm-password" name="password_confirmation" placeholder="Password"
+                            class="form-input" />
+                        @if ($errors->has('password_confirmation'))
+                        <span class="text-danger">{{ $errors->first('password_confirmation') }}</span>
+                        @endif
+                    </div>
+
+                    <input type="submit" value="Register" class="submit-button" />
+                </form>
+                <div class="login-link-container">
+                    <p>Already have an account? <a href="/login" class="login-link">Log in here.</a></p>
                 </div>
             </div>
+
+        </div>
+
+        <!-- Image Section -->
+        <div class="image-section">
+            <img class="background-image" src="{{ asset('assets/images/mental.jpg') }}" alt="Background" />
         </div>
     </div>
-    <!-- Bootstrap JS (optional) -->
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.bundle.min.js"></script>
+
 </body>
+
 </html>
+<script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"></script>
+<script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.5.4/dist/umd/popper.min.js"></script>
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
+
+
+

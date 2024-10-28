8c3d3c16af5c9cd8f0bec56cd2a9f9b67d4ccf18
Hasnain <zainsaqain@gmail.com>
2024-10-28 07:26:21 -0700

    layouts bani

---
+++ resources/views/mental/layouts/footer.blade.php
@@ -0,0 +1,63 @@
+<footer class="footer">
+    <div class="footer-top">
+        <div class="container  footer-top__divider">
+            <div class="row">
+                <div class="col-xs-12  col-md-4">
+                    <div class="widget  widget_text">
+                        <h6 class="footer-top__headings">A little something about us</h6>
+                        <div class="textwidget">
+                            <p>It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters.</p> <a href="about.html" class="read-more">READ MORE</a></div>
+                    </div>
+                </div>
+                <div class="col-xs-12  col-md-2">
+                    <div class="widget  widget_nav_menu">
+                        <h6 class="footer-top__headings">Navigation</h6>
+                        <div class="menu-footer-menu-container">
+                            <ul id="menu-footer-menu" class="menu">
+                                <li class="menu-item"><a href="services.html">Services</a></li>
+                                <li class="menu-item"><a href="about.html">About Me</a></li>
+                                <li class="menu-item"><a href="blog.html">Blog</a></li>
+                                <li class="menu-item"><a href="shop.html">Shop</a></li>
+                                <li class="menu-item"><a href="appointment.html">Make an Appointment</a></li>
+                                <li class="menu-item"><a href="contact.html">Contact Us</a></li>
+                            </ul>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-xs-12  col-md-2">
+                    <div class="widget  widget_nav_menu">
+                        <h6 class="footer-top__headings">Our Services</h6>
+                        <div class="menu-services-container">
+                            <ul id="menu-services" class="menu">
+                                <li class="menu-item"><a href="depression-treatment.html">Depression</a></li>
+                                <li class="menu-item"><a href="anxiety-treatment.html">Anxiety</a></li>
+                                <li class="menu-item">
+                                    <a href="couples-therapy.html">Couples</a>
+                                </li>
+                                <li class="menu-item"><a href="services/individual-treatment/index.html">Individual</a></li>
+                                <li class="menu-item"><a href="post-divorce-recovery.html">Post-divorce</a></li>
+                                <li class="menu-item"><a href="children-therapy.html">Children</a></li>
+                            </ul>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-xs-12  col-md-4">
+                    <div class="widget  widget_text">
+                        <h6 class="footer-top__headings">Buy Now</h6>
+                        <div class="textwidget">
+                            <p>If you come all the way down here, you probably really like our MentalPress theme. To save you all the troubles finding where to buy this theme, we have a solution for that too. Just click the button below.</p>
+                            <br> <a class="btn  btn-primary" href="http://themeforest.net/item/x/XXXXXX?ref=surjithctly" target="_blank">BUY NOW</a></div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="footer-bottom">
+        <div class="container">
+            <div class="footer-bottom__left"> <img src="{{asset('assets/images/logo_bw.png') }}" alt="Logo Footer"></div>
+            <div class="footer-bottom__right">
+                <div class="footer-bottom__payment-methods"> <span>PAYMENT METHODS:</span> &nbsp; &nbsp; &nbsp; <i class="fa  fa-3x  fa-cc-visa"></i> &nbsp; <i class="fa  fa-3x  fa-cc-mastercard"></i> &nbsp; <i class="fa  fa-3x  fa-cc-amex"></i> &nbsp; <i class="fa  fa-3x  fa-cc-paypal"></i></div>
+            </div>
+        </div>
+    </div>
+</footer>
---
+++ resources/views/mental/layouts/header.blade.php
@@ -0,0 +1,61 @@
+<div class="top">
+    <div class="top__background"></div>
+    <div class="container">
+        <div class="top__tagline"> Counseling and psychotherapy for individuals, couples and families</div>
+        <nav class="top__menu">
+            <ul id="menu-top-menu" class="top-navigation">
+                <li class="menu-item menu-item-has-children"><a href="extras.html">Extras</a>
+                    <ul class="sub-menu">
+                        <li class="menu-item"><a href="shortcodes.html">Shortcodes</a></li>
+                        <li class="menu-item"><a href="brochure-box.html">Brochure Box</a></li>
+                    </ul>
+                </li>
+                <li class="menu-item"><a href="http://themeforest.net/item/psychology-counseling-medical-website-template-mentalpress/14310650?ref=surjithctly&amp;utm_source=live_demo_link">Buy Now</a></li>
+                <li class="menu-item"><a target="_blank" href="https://demo.web3canvas.com/themeforest/docs/html/mentalpress/">Theme Documentation</a></li>
+            </ul>
+        </nav>
+    </div>
+</div>
+
+
+
+
+
+
+<div class="container">
+    <header class="header">
+        <div class="logo">
+            <a href="index.html"> <img src="{{asset('assets/images/logo.png') }}" alt="MentalPress" srcset="{{asset('assets/images/logo.png') }}, {{asset('assets/images/logo_2x.png 2x') }}" class="img-responsive" width="260" height="90"> </a>
+        </div>
+        <a href="#main-navigation" class="navbar-toggle"> <span class="navbar-toggle__text">MENU</span> <span class="navbar-toggle__icon-bar"> <span class="icon-bar"></span> <span class="icon-bar"></span> <span class="icon-bar"></span> </span>
+        </a>
+        <div class="header-widgets">
+            <div class="widget  header-widgets__widget  widget-icon-box">
+                <div class="icon-box"><i class="fa  fa-home  fa-3x"></i>
+                    <div class="icon-box__text">
+                        <h4 class="icon-box__title">227 Marion Street</h4>
+                        <span class="icon-box__subtitle">Columbia, SC 29201</span></div>
+                </div>
+            </div>
+            <div class="widget  header-widgets__widget  widget-icon-box">
+                <div class="icon-box"><i class="fa  fa-clock-o  fa-3x"></i>
+                    <div class="icon-box__text">
+                        <h4 class="icon-box__title">Mon - Sat 8.00 - 18.00</h4>
+                        <span class="icon-box__subtitle">Sunday CLOSED</span></div>
+                </div>
+            </div>
+
+            <div class="widget  header-widgets__widget  widget-social-icons">
+                <a class="social-icons__link" href="https://www.facebook.com/web3canvas" target="_blank"><i class="fa  fa-facebook"></i></a>
+                <a class="social-icons__link" href="https://twitter.com/web3canvas" target="_blank"><i class="fa  fa-twitter"></i></a>
+                <a class="social-icons__link" href="https://www.youtube.com/user/surjithctly" target="_blank"><i class="fa  fa-youtube"></i></a>
+            </div>
+
+            <div class="widget  header-widgets__widget  widget_text">
+                <div class="textwidget"><a class="btn  btn-danger  btn-rounded" href="tel:18881234567" target="_self"><i class="fa fa-phone"></i> 1-888-123-4567</a></div>
+
+            </div>
+
+        </div>
+    </header>
+</div>
